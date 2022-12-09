bdc52ce08732cee1401d5225e0a267f62f566092
Maurycy Zarzycki <maurycy@evidentlycube.com>
2022-12-09 18:23:51 +0100

    + Added `name` to plugin.info + Fixed indent character from spaces to tabs

--- plugin/tiddlers/closer.js
+++ plugin/tiddlers/closer.js
@@ -6,151 +6,151 @@ module-type: library
 
 (function () {
 
-    /*jslint node: true, browser: true */
-    /*global $tw: false */
-    "use strict";
+	/*jslint node: true, browser: true */
+	/*global $tw: false */
+	"use strict";
 
-    var Closer = function (editWidget, areaNode) {
-        // Only hook for tiddler's body input
-        if (editWidget.attributes.field !== 'text') {
-            return;
-        }
+	var Closer = function (editWidget, areaNode) {
+		// Only hook for tiddler's body input
+		if (editWidget.attributes.field !== 'text') {
+			return;
+		}
 
-        // About underlying Widget
-        this._areaNode = areaNode;
-        this._tryToComplete = false;
+		// About underlying Widget
+		this._areaNode = areaNode;
+		this._tryToComplete = false;
 
-        // Listen to the Keyboard
-        $tw.utils.addEventListeners(this._areaNode, [
-            { name: "keydown", handlerObject: this, handlerMethod: "handleKeyDown" },
-            { name: "input", handlerObject: this, handlerMethod: "handleInput" },
-        ]);
-    };
+		// Listen to the Keyboard
+		$tw.utils.addEventListeners(this._areaNode, [
+			{ name: "keydown", handlerObject: this, handlerMethod: "handleKeyDown" },
+			{ name: "input", handlerObject: this, handlerMethod: "handleInput" },
+		]);
+	};
 
-    function isWhitespace(char) {
-        return char === ' ' || char === "\t" || char === "\n" || char === "\r";
-    }
+	function isWhitespace(char) {
+		return char === ' ' || char === "\t" || char === "\n" || char === "\r";
+	}
 
-    function isTripleQuote(text, curPos) {
-        if (curPos < 3) {
-            return false;
-        }
+	function isTripleQuote(text, curPos) {
+		if (curPos < 3) {
+			return false;
+		}
 
-        return text.charAt(curPos) === '"'
-            && text.charAt(curPos - 1) === '"'
-            && text.charAt(curPos - 2) === '"';
-    }
+		return text.charAt(curPos) === '"'
+			&& text.charAt(curPos - 1) === '"'
+			&& text.charAt(curPos - 2) === '"';
+	}
 
-    Closer.prototype.handleKeyDown = function (event) {
-        this._tryToComplete = event.keyCode === '>'.charCodeAt(0) || event.key === '>';
-    }
+	Closer.prototype.handleKeyDown = function (event) {
+		this._tryToComplete = event.keyCode === '>'.charCodeAt(0) || event.key === '>';
+	}
 
-    Closer.prototype.handleInput = function (event) {
-        var curPos = this._areaNode.selectionStart;  // cursor position
-        var inputText = this._areaNode.value;   // text in the area
+	Closer.prototype.handleInput = function (event) {
+		var curPos = this._areaNode.selectionStart;  // cursor position
+		var inputText = this._areaNode.value;   // text in the area
 
-        if (!this._tryToComplete) {
-            return;
-        }
+		if (!this._tryToComplete) {
+			return;
+		}
 
-        this._tryToComplete = false;
+		this._tryToComplete = false;
 
-        var isFirstCharacter = true;
-        var wasLastWhitespace = false;
-        var braceDepth = 1;
-        var insideTripleQuote = false;
-        var insideSingleQuote = false;
-        var insideDoubleQuote = false;
-        var startedWithDoubleBrace = false;
-        for (var i = curPos - 2; i >= 0; i--) {
-            var char = inputText.charAt(i);
+		var isFirstCharacter = true;
+		var wasLastWhitespace = false;
+		var braceDepth = 1;
+		var insideTripleQuote = false;
+		var insideSingleQuote = false;
+		var insideDoubleQuote = false;
+		var startedWithDoubleBrace = false;
+		for (var i = curPos - 2; i >= 0; i--) {
+			var char = inputText.charAt(i);
 
-            if (isWhitespace(char)) {
-                wasLastWhitespace = true;
-                // Skip whitespaces
-                continue;
-            }
+			if (isWhitespace(char)) {
+				wasLastWhitespace = true;
+				// Skip whitespaces
+				continue;
+			}
 
-            if (insideTripleQuote && !isTripleQuote(inputText, i)) {
-                continue;
-            } else if (insideDoubleQuote && char !== '"') {
-                continue;
-            } else if (insideSingleQuote && char !== "'") {
-                continue;
-            } else if (insideSingleQuote || insideDoubleQuote || insideTripleQuote) {
-                insideSingleQuote = false;
-                insideDoubleQuote = false;
-                insideTripleQuote = false;
-                continue;
-            }
+			if (insideTripleQuote && !isTripleQuote(inputText, i)) {
+				continue;
+			} else if (insideDoubleQuote && char !== '"') {
+				continue;
+			} else if (insideSingleQuote && char !== "'") {
+				continue;
+			} else if (insideSingleQuote || insideDoubleQuote || insideTripleQuote) {
+				insideSingleQuote = false;
+				insideDoubleQuote = false;
+				insideTripleQuote = false;
+				continue;
+			}
 
-            if (char === "'") {
-                insideSingleQuote = true;
-                continue;
-            } else if (char === '"') {
-                if (isTripleQuote(inputText, i)) {
-                    insideTripleQuote = true;
-                    i -= 2;
-                    continue;
-                }
+			if (char === "'") {
+				insideSingleQuote = true;
+				continue;
+			} else if (char === '"') {
+				if (isTripleQuote(inputText, i)) {
+					insideTripleQuote = true;
+					i -= 2;
+					continue;
+				}
 
-                insideDoubleQuote = true;
-                continue;
-            }
+				insideDoubleQuote = true;
+				continue;
+			}
 
-            if (char === '/' && isFirstCharacter) {
-                isFirstCharacter = false;
-                // Self-closed tag, skip
-                break;
-            } else if (char === '/' && i > 0 && inputText.charAt(i - 1) === '<') {
-                isFirstCharacter = false;
-                // Found some other tag that's being closed, which means we totally should not be closing right now
-                break;
-            } else if (isFirstCharacter && char === '>') {
-                startedWithDoubleBrace = true;
-            }
+			if (char === '/' && isFirstCharacter) {
+				isFirstCharacter = false;
+				// Self-closed tag, skip
+				break;
+			} else if (char === '/' && i > 0 && inputText.charAt(i - 1) === '<') {
+				isFirstCharacter = false;
+				// Found some other tag that's being closed, which means we totally should not be closing right now
+				break;
+			} else if (isFirstCharacter && char === '>') {
+				startedWithDoubleBrace = true;
+			}
 
-            isFirstCharacter = false;
+			isFirstCharacter = false;
 
-            if (char === '>') {
-                braceDepth++;
-            } else if (char === '<') {
-                braceDepth--;
-            }
+			if (char === '>') {
+				braceDepth++;
+			} else if (char === '<') {
+				braceDepth--;
+			}
 
-            if (braceDepth === 0) {
-                var wordLength = inputText.slice(i + 1, curPos).search(/[^$a-zA-Z0-9_]+/);
-                var word = inputText.substr(i + 1, wordLength !== -1 ? wordLength : curPos - 1 - 1);
-                // Handle closing <<>> structure
-                if (i > 0 && inputText.charAt(i - 1) === '<') {
-                    break;
-                } else if (startedWithDoubleBrace && inputText.charAt(i + 1) === '<') {
-                    break;
-                }
+			if (braceDepth === 0) {
+				var wordLength = inputText.slice(i + 1, curPos).search(/[^$a-zA-Z0-9_]+/);
+				var word = inputText.substr(i + 1, wordLength !== -1 ? wordLength : curPos - 1 - 1);
+				// Handle closing <<>> structure
+				if (i > 0 && inputText.charAt(i - 1) === '<') {
+					break;
+				} else if (startedWithDoubleBrace && inputText.charAt(i + 1) === '<') {
+					break;
+				}
 
-                var wordLowercase = word.toLowerCase();
-                // Do not trigger for empty tags
-                if (word.length === 0) {
-                    break;
-                // void elements don't need to be closed
-                } else if (wordLowercase === 'br' || wordLowercase === 'input' || wordLowercase === 'hr' || wordLowercase === 'img') {
-                    break;
-                }
+				var wordLowercase = word.toLowerCase();
+				// Do not trigger for empty tags
+				if (word.length === 0) {
+					break;
+				// void elements don't need to be closed
+				} else if (wordLowercase === 'br' || wordLowercase === 'input' || wordLowercase === 'hr' || wordLowercase === 'img') {
+					break;
+				}
 
-                var toInsert = '</' + word + '>';
+				var toInsert = '</' + word + '>';
 
-                this._areaNode.focus();
-                this._areaNode.selectionStart = curPos;
-                this._areaNode.selectionEnd = curPos;
+				this._areaNode.focus();
+				this._areaNode.selectionStart = curPos;
+				this._areaNode.selectionEnd = curPos;
 
-                this._areaNode.ownerDocument.execCommand('insertText', false, toInsert);
-                this._areaNode.selectionStart = this._areaNode.selectionEnd = curPos;
-                break;
-            }
-        }
-    };
+				this._areaNode.ownerDocument.execCommand('insertText', false, toInsert);
+				this._areaNode.selectionStart = this._areaNode.selectionEnd = curPos;
+				break;
+			}
+		}
+	};
 
-    exports.Closer = Closer;
+	exports.Closer = Closer;
 
 })();
 
